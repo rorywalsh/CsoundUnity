7984b7cfd222ef5a5f7faf7543ca5330cd4bd1d6
rorywalsh <rorywalsh@ear.ie>
2016-05-28 15:35:01 +0100

    added mechanism to read vales from identifiers in standalone mode, refactored the parseCsdFile method and added it to the CsoundUnity class

--- CsoundUnityScripts/CsoundUnity.cs
+++ CsoundUnityScripts/CsoundUnity.cs
@@ -16,7 +16,27 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
+
+//utility class for controller and channels
+public class CsoundChannelController
+{
+    public string type = "", channel = "", text = "", caption= "";
+    public float min, max, value, skew, increment;
+
+    public void setRange(float uMin, float uMax, float uValue)
+    {
+        min = uMin;
+        max = uMax;
+        value = uValue;
+    }
+
+}
+
 
 /*
  * CsoundUnity class
@@ -55,6 +75,8 @@ public class CsoundUnity : MonoBehaviour
     private bool compiledOk = false;
     public bool mute = false;
     public bool processClipAudio = false;
+    //structure to hold channel data
+    List<CsoundChannelController> channels;
 
     /**
      * CsoundUnity Awake function. Called when this script is first instantiated. This should never be called directly. 
@@ -73,6 +95,7 @@ public class CsoundUnity : MonoBehaviour
         string csoundFilePath = Application.streamingAssetsPath + "/" + csoundFile + "_";
         string dataPath = Application.streamingAssetsPath;
         System.Environment.SetEnvironmentVariable("Path", Application.streamingAssetsPath);
+        channels = new List<CsoundChannelController>();
         /*
          * the CsoundUnity constructor takes a path to the project's Data folder, and path to the file name.
          * It then calls createCsound() to create an instance of Csound and compile the 'csdFile'. 
@@ -80,6 +103,15 @@ public class CsoundUnity : MonoBehaviour
          * Csound on a string channel. This means we can then load samples contained within that folder.
          */
         csound = new CsoundUnityBridge(dataPath, csoundFilePath);
+
+        channels = parseCsdFile(csoundFilePath);
+        //initialise channels if found in xml descriptor..
+        for(int i=0;i<channels.Count;i++)
+        {
+            //print("Channel:"+channels[i].channel + " Value:" + channels[i].value.ToString());
+            csound.setChannel(channels[i].channel, channels[i].value);
+        }
+
 
         /*
          * This method prints the Csound output to the Unity console
@@ -154,7 +186,9 @@ public class CsoundUnity : MonoBehaviour
                     //write output from Csound to AudioSource
                     samples[i] = (float)(getSample(ksmpsIndex, 0) / zerdbfs);
                     if (numChannels == 2)
-                        samples[i] = (float)(getSample(ksmpsIndex + 1, 1) / zerdbfs);
+                    {
+                        samples[i+1] = (float)(getSample(ksmpsIndex + 1, 1) / zerdbfs);
+                    }
                 }
             }
         }
@@ -203,10 +237,12 @@ public class CsoundUnity : MonoBehaviour
     /**
     * process a ksmps-sized block of samples
     */
+#if UNITY_EDITOR
     public string getFilePath(Object obj)
     {
         return Application.dataPath.Replace("Assets", "") + AssetDatabase.GetAssetPath(obj);
     }
+#endif
     /**
      * map float within one range to another 
      */
@@ -264,5 +300,72 @@ public class CsoundUnity : MonoBehaviour
         for (int i = 0; i < csound.getCsoundMessageCount(); i++)
             print(csound.getCsoundMessage());
     }
+
+
+    public List<CsoundChannelController> parseCsdFile(string filename)
+    {
+        string[] fullCsdText = File.ReadAllLines(filename);
+        List<CsoundChannelController> locaChannelControllers;
+        locaChannelControllers = new List<CsoundChannelController>();
+
+        foreach (string line in fullCsdText)
+        {
+            if (line.Contains("</"))
+                break;
+
+            string newLine = line;
+            string control = line.Substring(0, line.IndexOf(" ") > -1 ? line.IndexOf(" ") : 0);
+            if (control.Length > 0)
+                newLine = newLine.Replace(control, "");
+
+
+            if (control.Contains("slider") || control.Contains("button") || control.Contains("checkbox") || control.Contains("groupbox") || control.Contains("form"))
+            {
+                CsoundChannelController controller = new CsoundChannelController();
+                controller.type = control;
+
+                if (line.IndexOf("caption(") > -1)
+                {
+                    string infoText = line.Substring(line.IndexOf("caption(") + 9);
+                    infoText = infoText.Substring(0, infoText.IndexOf(")") - 1);
+                    controller.caption = infoText;
+                }
+
+                if (line.IndexOf("text(") > -1)
+                {
+                    string text = line.Substring(line.IndexOf("text(") + 6);
+                    text = text.Substring(0, text.IndexOf(")") - 1);
+                    controller.text = text;
+                }
+
+                if (line.IndexOf("channel(") > -1)
+                {
+                    string channel = line.Substring(line.IndexOf("channel(") + 9);
+                    channel = channel.Substring(0, channel.IndexOf(")") - 1);
+                    controller.channel = channel;
+                }
+
+                if (line.IndexOf("range(") > -1)
+                {
+                    string range = line.Substring(line.IndexOf("range(") + 6);
+                    range = range.Substring(0, range.IndexOf(")"));
+                    char[] delimiterChars = { ',' };
+                    string[] tokens = range.Split(delimiterChars);
+                    controller.setRange(float.Parse(tokens[0]), float.Parse(tokens[1]), float.Parse(tokens[2]));
+                }
+
+                if (line.IndexOf("value(") > -1)
+                {
+                    string value = line.Substring(line.IndexOf("value(") + 6);
+                    value = value.Substring(0, value.IndexOf(")"));
+                    controller.value = value.Length > 0 ? float.Parse(value) : 0;
+                }
+
+                locaChannelControllers.Add(controller);
+            }
+        }
+        return locaChannelControllers;
+    }
+
 }
 
---
+++ Editor/CsoundUnityEditor.cs
@@ -0,0 +1,156 @@
+﻿/*
+Copyright (c) <2016> Rory Walsh
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.Collections.Generic;
+
+
+[CustomEditor(typeof(CsoundUnity))]
+[System.Serializable]
+public class CsoundUnityEditor : Editor
+{
+    CsoundUnity csoundUnity;
+    string infoText;
+    List<float> controllerValues;
+    List<CsoundChannelController> channelControllers;
+
+    public static CsoundUnityEditor window;
+    bool initPass = true;
+
+
+    void OnEnable()
+    {
+        csoundUnity = (CsoundUnity)target;
+        channelControllers = new List<CsoundChannelController>();
+        controllerValues = new List<float>();
+
+        //parse Csound files for CsoundUnity descriptor
+        if (csoundUnity.csoundFile.Length > 4)
+        {
+            //deals with Csound files found the CsoundFiles folder
+            string dir = Application.dataPath + "/Scripts/CsoundFiles";
+            if (Directory.Exists(dir))
+                channelControllers = csoundUnity.parseCsdFile(dir+"/"+csoundUnity.csoundFile);
+            else
+                channelControllers = csoundUnity.parseCsdFile(Application.dataPath + "Scripts/" + csoundUnity.csoundFile);
+        }
+
+    } 
+
+    public override void OnInspectorGUI()
+    {
+        GUI.skin = (GUISkin)(AssetDatabase.LoadAssetAtPath("Assets/Editor/CsoundUnity.guiskin", typeof(GUISkin)));
+
+        //get caption info first
+        for (int i = 0; i < channelControllers.Count; i++)
+        {
+            if (channelControllers[i].type.Contains("form"))
+            {
+                infoText = channelControllers[i].caption;
+            }
+        }
+
+        EditorGUILayout.HelpBox(infoText, MessageType.None);
+        GUI.SetNextControlName("CsoundfileTextField");
+        csoundUnity.csoundFile = EditorGUILayout.TextField("Csound file", csoundUnity.csoundFile);
+        csoundUnity.processClipAudio = EditorGUILayout.Toggle("Process Clip Audio", csoundUnity.processClipAudio);
+        csoundUnity.mute = EditorGUILayout.Toggle("Mute Csound", csoundUnity.mute);
+        csoundUnity.logCsoundOutput = EditorGUILayout.Toggle("Log Csound Output", csoundUnity.logCsoundOutput);
+
+        //create drag and drop area for Csound files
+        DropAreaGUI();
+
+        //create controls for each Csound channel found in the file descriptor
+        for (int i=0;i<channelControllers.Count;i++)
+        {
+            controllerValues.Add(channelControllers[i].value);
+            string label = channelControllers[i].text.Length > 3 ? channelControllers[i].text : channelControllers[i].channel;
+            if (channelControllers[i].type.Contains("slider"))
+            {                
+                controllerValues[i] = EditorGUILayout.Slider(label, controllerValues[i], channelControllers[i].min, channelControllers[i].max);
+                if (controllerValues[i] != channelControllers[i].value || initPass)
+                {
+                    channelControllers[i].value = controllerValues[i];
+                    if (Application.isPlaying)
+                        csoundUnity.setChannel(channelControllers[i].channel, controllerValues[i]);
+                }
+            }
+            else if (channelControllers[i].type.Contains("button"))
+            {
+                if (GUILayout.Button(label))
+                {
+                    channelControllers[i].value = channelControllers[i].value == 1 ? 0 : 1;
+                    csoundUnity.setChannel(channelControllers[i].channel, channelControllers[i].value);
+                }
+            }
+            else if (channelControllers[i].type.Contains("groupbox"))
+            {
+                EditorGUILayout.HelpBox(channelControllers[i].text, MessageType.None);
+            }
+            else if (channelControllers[i].type.Contains("checkbox"))
+            {
+                controllerValues[i] = EditorGUILayout.Toggle(label, controllerValues[i]==1 ? true : false)==true ? 1 : 0;
+                if (controllerValues[i] != channelControllers[i].value)
+                {
+                    channelControllers[i].value = controllerValues[i];
+                    if (Application.isPlaying)
+                        csoundUnity.setChannel(channelControllers[i].channel, controllerValues[i]);
+                }
+            }
+        }
+        initPass = false;
+
+
+
+    }
+
+    public void DropAreaGUI()
+    {
+        Event evt = Event.current;
+        Rect drop_area = GUILayoutUtility.GetRect(0.0f, 20.0f, GUILayout.ExpandWidth(true));
+        GUI.Box(drop_area, "Drag and Drop Csound file here");
+
+        switch (evt.type)
+        {
+            case EventType.DragUpdated:
+            case EventType.DragPerform:
+                if (!drop_area.Contains(evt.mousePosition))
+                    return;
+
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                if (evt.type == EventType.DragPerform)
+                {
+                    DragAndDrop.AcceptDrag();
+
+                    foreach (string dragged_object in DragAndDrop.paths)
+                    {
+                        csoundUnity.csoundFile = Path.GetFileName(dragged_object);
+                        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+                        if(csoundUnity.csoundFile.Length>4)
+                            channelControllers = csoundUnity.parseCsdFile(dragged_object);
+                            
+                    }
+                }
+                break;
+        }
+    }
+
+}
+
+
