9006bec330940747db1fac4bf2a9c4b07ee0e9bb
gb <giovanni.bedetti@gmail.com>
2021-11-03 19:49:05 +0000

    Added Partikkel sample
    Removed some useless meta files

---
+++ Samples~/GranularSynthesis/Scripts/ParametersController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Csound.GranularSynthesis.Partikkel
+{
+    [RequireComponent(typeof(CsoundUnity))]
+    public class ParametersController : MonoBehaviour
+    {
+        [Tooltip("The prefab to be used as ParameterSetter. Requires a ParameterSetter script")]
+        [SerializeField] GameObject _parameterCubePrefab;
+        [Tooltip("The area to which the parameters will be remapped to")]
+        [SerializeField] Vector2 _parametersAreaSizes;
+        [Tooltip("The player position will be used to update the graphic appearance of ParameterSetters")]
+        [SerializeField] GameObject _player;
+
+        CsoundUnity _csound;
+
+        void Start()
+        {
+            _csound = GetComponent<CsoundUnity>();
+            var chanList = _csound.channels;
+            for (var i = 0; i < chanList.Count; i += 2)
+            {
+                var paramGO = Instantiate(_parameterCubePrefab);
+                var setter = paramGO.GetComponent<ParameterSetter>();
+                var minPosition = new Vector2(-_parametersAreaSizes.x / 2, -_parametersAreaSizes.y / 2);
+                var maxPosition = new Vector2(_parametersAreaSizes.x / 2, _parametersAreaSizes.y / 2);
+                // each prefab will manage two channels, one will be controlled by the position on the x axis and the other on the z axis
+                // look just for channels of the type "slider"
+                while (!chanList[i].type.Contains("slider"))
+                {
+                    i++;
+                    if (i >= chanList.Count) break;
+                }
+                // do some checks for the second channel, check for index overflow or if it's not a slider: in those case it will be null
+                var zChan = (i + 1) >= chanList.Count ? null : !chanList[i + 1].type.Contains("slider") ? null : chanList[i + 1];
+                setter.Init(_csound, _player, chanList[i], zChan, minPosition, maxPosition);
+            }
+        }
+    }
+}
---
+++ Samples~/GranularSynthesis/Scripts/WaveformDrawer.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Csound.GranularSynthesis.Partikkel
+{
+    [RequireComponent(typeof(CsoundUnity))]
+    public class WaveformDrawer : MonoBehaviour
+    {
+        [Tooltip("The raw image where the waveform will be drawn")]
+        [SerializeField] RawImage _targetImage;
+        [Tooltip("The audioclip that will be used as a source")]
+        [SerializeField] AudioClip _clip;
+        [Tooltip("The progress bar that will be updated using a Csound Control Channel")]
+        [SerializeField] Image _progressBar;
+
+        private RectTransform _progressBarRT;
+        private float _targetImageWidth;
+        private CsoundUnity _csound;
+
+        void Start()
+        {
+            _targetImage.texture = PaintWaveformSpectrum(_clip, 1, 512, 128, Color.white);
+            _progressBarRT = _progressBar.GetComponent<RectTransform>();
+            _targetImageWidth = _targetImage.GetComponent<RectTransform>().rect.width;
+            _csound = GetComponent<CsoundUnity>();
+        }
+
+        void Update()
+        {
+            _progressBarRT.anchoredPosition = new Vector2((float)_csound.GetChannel("samplepos") * _targetImageWidth, 0);
+        }
+
+        public Texture2D PaintWaveformSpectrum(AudioClip audio, float saturation, int width, int height, Color col)
+        {
+            Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            float[] samples = new float[audio.samples];
+            float[] waveform = new float[width];
+            audio.GetData(samples, 0);
+            int packSize = (audio.samples / width) + 1;
+            int s = 0;
+            for (int i = 0; i < audio.samples; i += packSize)
+            {
+                waveform[s] = Mathf.Abs(samples[i]);
+                s++;
+            }
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    tex.SetPixel(x, y, Color.black);
+                }
+            }
+
+            for (int x = 0; x < waveform.Length; x++)
+            {
+                for (int y = 0; y <= waveform[x] * ((float)height * .75f); y++)
+                {
+                    tex.SetPixel(x, (height / 2) + y, col);
+                    tex.SetPixel(x, (height / 2) - y, col);
+                }
+            }
+            tex.Apply();
+
+            return tex;
+        }
+    }
+}
