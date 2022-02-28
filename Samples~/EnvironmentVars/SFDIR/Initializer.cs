82b06e2a483e0476dab823754351bf005e0388c4
gb <giovanni.bedetti@gmail.com>
2022-02-28 19:22:08 +0000

    added SFDIR sample in EnvironmentVars

---
+++ Samples~/EnvironmentVars/SFDIR/Initializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Csound.EnvironmentVars
+{
+    public class Initializer : MonoBehaviour
+    {
+        [SerializeField] private string[] _soundFontsNames;
+        [SerializeField] private CsoundUnity _csoundUnityPrefab;
+        private CsoundUnity _csoundUnityInstance;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            foreach (var sfName in _soundFontsNames)
+            {
+                var dir = Path.Combine(Application.persistentDataPath, "CsoundFiles");
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+
+                var destinationPath = Path.Combine(dir, sfName + ".sf2");
+                if (!File.Exists(destinationPath))
+                {
+                    var sf = Resources.Load<TextAsset>(sfName);
+                    Debug.Log($"Writing sf file at path: {destinationPath}");
+                    File.WriteAllText(destinationPath, sf.text);
+                }
+            }
+            // if you need to do something with Csound, use this instance!
+            _csoundUnityInstance = Instantiate(_csoundUnityPrefab);
+        }
+    }
+}
