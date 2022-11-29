954a5a57fdfa59e7d35012c069631a88e1918263
gb <giovanni.bedetti@gmail.com>
2022-11-29 22:46:42 +0000

    Added CsoundUnityPostProcessBuild
    This script fixes the generated iOS project to be able to build it out of the box

---
+++ Editor/CsoundUnityPostProcessBuild.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class CsoundUnityPostProcessBuild
+{
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+        if (target == BuildTarget.iOS)
+        {
+            var projectPath = PBXProject.GetPBXProjectPath(path);
+            var project = new PBXProject();
+            project.ReadFromString(File.ReadAllText(projectPath));
+            var targetGUID = project.GetUnityFrameworkTargetGuid();
+            project.AddFrameworkToProject(targetGUID, "Accelerate.framework", false);
+            project.AddBuildProperty(targetGUID, "OTHER_CODE_SIGN_FLAGS", "--generate-entitlement-der");
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
+    }
+}
