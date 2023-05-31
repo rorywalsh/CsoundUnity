9815e835cc3e5bfe1cf4d5781e5eb0633293bdd6
gb <giovanni.bedetti@gmail.com>
2023-05-31 23:16:23 +0100

    improved Remap method
    now it is possible to set the skew with two different modes

--- Runtime/CsoundUnity.cs
+++ Runtime/CsoundUnity.cs
@@ -1399,21 +1399,56 @@ namespace Csound.Unity
             return csound.GetCurrentTimeSamples();
         }
 
+        [Serializable]
+        public enum SkewMode { Cabbage, Normalized };
+
         /// <summary>
-        /// Linear remap floats within one range to another
+        /// Remaps a float value from one range to another with optional clamping and skewing. 
+        /// There are two possible modes for skewing: Cabbage and Normalized.
+        /// <para>
+        /// In Cabbage mode, the skew factor has no upper limit and the mapping behaves as follows:
+        /// - Exponential mapping for values above 0 to 1
+        /// - Linear mapping at 1
+        /// - Logarithmic mapping for values above 1
+        /// </para>
+        /// <para>
+        /// In Normalized mode, the skew factor is bound between 0 and 1, and the mapping behaves as follows:
+        /// - Exponential mapping from values above 0 to 0.5
+        /// - Linear mapping at 0.5
+        /// - Logarithmic mapping for values above 0.5 to 1
+        /// </para>
+        /// <para>
+        /// Both modes return the lower target bound (from2) when the skew is 0 or below.
+        /// The Normalized mode returns the upper target bound (to2) when the skew is 1 or above.
+        /// </para>
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="from1"></param>
-        /// <param name="to1"></param>
-        /// <param name="from2"></param>
-        /// <param name="to2"></param>
-        /// <returns></returns>
-        public static float Remap(float value, float from1, float to1, float from2, float to2, bool clamp = false)
+        /// <param name="value">The input value to be remapped.</param>
+        /// <param name="from1">The lower bound of the source range.</param>
+        /// <param name="to1">The upper bound of the source range.</param>
+        /// <param name="from2">The lower bound of the target range.</param>
+        /// <param name="to2">The upper bound of the target range.</param>
+        /// <param name="clamp">Determines whether the mapped value should be clamped within the target range. Default is false.</param>
+        /// <param name="skew">A skew factor that controls the mapping behavior. Default is 1</param>
+        /// <param name="mode">The mode for skewing the mapping behavior. Default is Cabbage</param>
+        /// <returns>The remapped value within the target range.</returns>
+        public static float Remap(float value, float from1, float to1, float from2, float to2, bool clamp = false, float skew = 1f, SkewMode mode = SkewMode.Cabbage)
         {
-            float retValue = (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+            if (skew <= 0f) return from2;
+
+            if (mode == SkewMode.Normalized)
+            {
+                if (skew >= 1f) return to2;
+            }
+
+            var normalizedValue = (value - from1) / (to1 - from1);
+            var exponent = mode == SkewMode.Cabbage ? 1f / skew : Mathf.Log(skew) / Mathf.Log(0.5f);
+            var factor = Mathf.Pow(normalizedValue, exponent);
+            var retValue = Mathf.Lerp(from2, to2, factor);
+
             if (float.IsNaN(retValue)) return from2;
             if (float.IsPositiveInfinity(retValue)) return to2;
             if (float.IsNegativeInfinity(retValue)) return from2;
+
             return clamp ? Mathf.Clamp(retValue, from2, to2) : retValue;
         }
 
