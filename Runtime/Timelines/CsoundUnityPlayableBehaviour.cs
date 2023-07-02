c352035deb6ca4732b4f06eca80f786c390937a3
gb <giovanni.bedetti@gmail.com>
2023-07-02 23:16:42 +0100

    added copyright for timeline scripts
    also some cleaning

--- Runtime/Timelines/CsoundUnityPlayableBehaviour.cs
+++ Runtime/Timelines/CsoundUnityPlayableBehaviour.cs
@@ -1,4 +1,31 @@
-﻿#if USE_TIMELINES
+﻿/*
+Copyright (C) 2015 Rory Walsh. 
+
+This file is part of CsoundUnity: https://github.com/rorywalsh/CsoundUnity
+
+This interface would not have been possible without Richard Henninger's .NET interface to the Csound API.
+
+Contributors:
+
+Bernt Isak Wærstad
+Charles Berman
+Giovanni Bedetti
+Hector Centeno
+NPatch
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#if USE_TIMELINES
 
 using System.Collections;
 using System.Collections.Generic;
@@ -14,12 +41,12 @@ namespace Csound.Unity.Timelines
         public string channel = "";
         public float value = 0f;
 
-        private CsoundUnity csound;
-        private bool firstFrameHappened;
-        private float defaultValue;
-
         // Keep this for reference, this is the basic behaviour
         // without the ability of mixing clips
+
+        //private CsoundUnity csound;
+        //private bool firstFrameHappened;
+        //private float defaultValue;
 
         //public override void ProcessFrame(Playable playable, FrameData info, object playerData)
         //{
