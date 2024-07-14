16365a09542420eeb56c4550c46d764327ebb7c2
gb <giovanni.bedetti@gmail.com>
2024-07-14 23:13:15 +0200

    added webgl sample

---
+++ Samples~/WebGL/TestWebGL/Scripts/MobileJoystick.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Csound.Unity.Samples.TestWebGL
+{
+    /// <summary>
+    /// A simple Joystick handle. 
+    /// Put this script in a gameObject with a graphic element with Raycast target enabled
+    /// </summary>
+    public class MobileJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public const float speedMul = 75.0f;
+
+        public delegate void Moved(Vector2 dir);
+        /// <summary>
+        /// Subscribe to this event to receive a clamped (-1, 1) Vector2 based on the handle position.
+        /// </summary>
+        public event Moved MovedEvent;
+
+        [SerializeField] private bool _pressed;
+        private int _touchId;
+        private Vector2 _defaultPos;
+        private Vector2 _startPos;
+        private RectTransform _rt;
+
+        private void Start()
+        {
+            this._rt = this.gameObject.GetComponent<RectTransform>();
+            this._defaultPos = this._rt.position;
+        }
+
+        private void Update()
+        {
+            if (this._pressed)
+            {
+#if !UNITY_EDITOR
+                if (this._touchId < 0 || this._touchId >= Input.touches.Length) return;
+                this._rt.position = Input.touches[this._touchId].position;
+#else
+                this._rt.position = Input.mousePosition;
+#endif
+
+                var dir = new Vector2(Mathf.Clamp((this._rt.position.x - this._defaultPos.x) / speedMul, -1.0f, 1.0f),
+                                      Mathf.Clamp((this._rt.position.y - this._defaultPos.y) / speedMul, -1.0f, 1.0f));
+
+                //invoke the event when moved
+                MovedEvent?.Invoke(dir);
+                Debug.Log(dir);
+            }
+            else
+            {
+                //restore the default position
+                this._rt.position = this._defaultPos;
+                //invoke the moved event with a zero Vector2
+                MovedEvent?.Invoke(Vector2.zero);
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            //Debug.Log("OnPointerDown");
+            if (this._pressed) return;
+
+            this._pressed = true;
+            this._touchId = eventData.pointerId;
+            this._startPos = eventData.position;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            //Debug.Log("OnPointerUp");
+            if (eventData.pointerId != this._touchId) return;
+
+            this._pressed = false;
+            this._touchId = -1;
+            this._startPos = Vector2.zero;
+        }
+    }
+}
---
+++ Samples~/WebGL/TestWebGL/Scripts/PlayerController.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Csound.Unity.Samples.TestWebGL
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Camera lookCamera;
+        
+        //public variables to control movement
+        public float speed = 3.0F;
+        public float jumpSpeed = 15;
+        public int gravity = 20;
+        public float rotationSensitivity = 10f;
+        public float maxYAngle = 80f;
+
+        // joysticks for mobile devices
+        public MobileJoystick movementJoystick;
+        public MobileJoystick lookJoystick;
+
+        public Button mobileButton;
+
+        public bool IsMobile { get { return Application.isMobilePlatform && Input.touchSupported; } }
+
+        private Vector2 currentRotation;
+
+        private CharacterController charController;
+        private Vector3 moveDirection;
+        private Vector2 movementJoystickDirection;
+        private Vector2 lookJoystickDirection;
+
+        private float VerticalAxis
+        {
+            get
+            {
+                return IsMobile ? movementJoystickDirection.y : Input.GetAxis("Vertical");
+            }
+        }
+
+        private float HorizontalAxis
+        {
+            get
+            {
+                return IsMobile ? movementJoystickDirection.x : Input.GetAxis("Horizontal");
+            }
+        }
+
+        private Quaternion LookRotation
+        {
+            get
+            {
+                if (IsMobile)
+                {
+                    currentRotation.x += lookJoystickDirection.x * rotationSensitivity;
+                    currentRotation.y -= lookJoystickDirection.y * rotationSensitivity;
+                }
+                else
+                {
+                    currentRotation.x += Input.GetAxis("Mouse X") * rotationSensitivity;
+                    currentRotation.y -= Input.GetAxis("Mouse Y") * rotationSensitivity;
+
+                }
+                currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+                currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+                var target = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
+                return target;
+            }
+        }
+
+        void Awake()
+        {
+            charController = GetComponent<CharacterController>();
+            //assign csoundUnity member
+            //csoundUnity = GetComponent<CsoundUnity>();
+
+            Debug.Log("IsMobile: " + IsMobile);
+
+            if (movementJoystick)
+            {
+                movementJoystick.transform.parent.parent.gameObject.SetActive(IsMobile);
+            }
+
+            if (IsMobile)
+            {
+                mobileButton.onClick.AddListener(Jump);
+            }
+        }
+
+        private void Start()
+        {
+            if (IsMobile)
+            {
+                if (movementJoystick) { movementJoystick.MovedEvent += MovementJoystick_MovedEvent; }
+                if (lookJoystick) { lookJoystick.MovedEvent += LookJoystick_MovedEvent; }
+                if (mobileButton) { mobileButton.onClick.AddListener(Jump); }
+            }
+        }
+
+        private void MovementJoystick_MovedEvent(Vector2 dir)
+        {
+            //Debug.Log("MovementJoystick Moved: " + dir.x);
+            movementJoystickDirection = dir;
+        }
+        private void LookJoystick_MovedEvent(Vector2 dir)
+        {
+            lookJoystickDirection = dir;
+        }
+
+
+        void Update()
+        {
+            // Handle rotation
+            Quaternion lookRotation = LookRotation; 
+            transform.eulerAngles = new Vector2(0, LookRotation.eulerAngles.y);
+            lookCamera.transform.localRotation = Quaternion.Euler(LookRotation.eulerAngles.x, 0, 0);
+            // Handle movement
+            if (charController.isGrounded)
+            {
+                moveDirection = new Vector3(HorizontalAxis, 0, VerticalAxis);
+                moveDirection = transform.TransformDirection(moveDirection) * speed;
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveDirection.y = jumpSpeed;
+                }
+            }
+            else
+            {
+                Vector3 moveDirectionTemp = new Vector3(HorizontalAxis, moveDirection.y, VerticalAxis);
+                moveDirectionTemp = transform.TransformDirection(moveDirectionTemp);
+                moveDirection.x = moveDirectionTemp.x * speed;
+                moveDirection.z = moveDirectionTemp.z * speed;
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            charController.Move(moveDirection * Time.deltaTime);
+        }
+
+        private void Jump()
+        {
+            moveDirection.y = jumpSpeed;
+        }
+    }
+}
