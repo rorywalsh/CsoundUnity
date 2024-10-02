51788bc9b648f7db5a06f2c0d091c196c172635b
gb <giovanni.bedetti@gmail.com>
2024-10-02 12:49:58 +0200

    updates to TableLoader to handle multichannel files

--- Runtime/Utilities/Components/TableLoader.cs
+++ Runtime/Utilities/Components/TableLoader.cs
@@ -9,10 +9,23 @@ namespace Csound.Unity.Utilities.MonoBehaviours
     /// </summary>
     public class TableLoader : MonoBehaviour
     {
-        [Tooltip("The audio clip from which the samples will be loaded")]
+        [Tooltip("The AudioClip from which the samples will be loaded")]
         [SerializeField] AudioClip _source;
+        
         [Tooltip("The Csound table number that will be created and filled with the loaded samples")]
         [SerializeField] int _tableNumber = 100;
+       
+        [Tooltip("If true it will only get the left channel of the source AudioClip, otherwise it will read all channels")]
+        [SerializeField] bool _forceToMono = true;
+
+        [Tooltip("The channel that will be read when forceToMono is true")]
+        [SerializeField] int _channel = 0;
+
+        [Tooltip("If true, when more than one channel is present in the source AudioClip, it will create a table for each channel, " + 
+            "starting from tableNumber and increasing by one for each channel.\n" +
+            "If false it will create an interleaved table with all the channels together. The number of channels will be written in the first index of the table")]    
+        [SerializeField] bool _splitChannels = true;
+
         [Tooltip("Specify the instance of Csound where the table will be created. " +
             "If empty, the CsoundUnity component will be searched in this GameObject")]
         [SerializeField] CsoundUnity _csound;
@@ -34,11 +47,38 @@ namespace Csound.Unity.Utilities.MonoBehaviours
                 yield return null; //waiting for initialization
             }
 
-            var samples = ASU.GetMonoSamples(_source);
-            Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: samples loaded: {samples.Length}, creating table #{_tableNumber}");
-            var res = _csound.CreateTable(_tableNumber, samples);
-            if (res == 0) Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: Created table {_tableNumber}!");
-            else Debug.LogError("Csound.Unity.Utilities.LoadFiles.TableLoader: Cannot create table");
+            if (_forceToMono) // only reading one channel from the source AudioClip
+            {
+                var monoSamples = ASU.GetMonoSamples(_source);
+                Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: samples loaded: {monoSamples.Length}, creating table #{_tableNumber}");
+                var resMono = _csound.CreateTable(_tableNumber, monoSamples);
+                if (resMono == 0) Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: Created table {_tableNumber}!");
+                else Debug.LogError($"Csound.Unity.Utilities.LoadFiles.TableLoader: Cannot create table {_tableNumber}");
+            }
+            else
+            {
+                if (_splitChannels) // creating a table for each channel found in the source AudioClip
+                {
+                    for(var i = 0; i < _source.channels; i++)
+                    {
+                        // get mono samples per channel
+                        var channelSamples = ASU.GetMonoSamples(_source, i);
+                        var tableNumber = _tableNumber + i;
+                        Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: samples loaded: {channelSamples.Length}, creating table #{tableNumber}");
+                        var resChannel = _csound.CreateTable(tableNumber, channelSamples);
+                        if (resChannel == 0) Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: Created table {tableNumber}!");
+                        else Debug.LogError($"Csound.Unity.Utilities.LoadFiles.TableLoader: Cannot create table {tableNumber}");
+                    }
+                }
+                else // creating interleaved table
+                {
+                    var interleavedSamples = ASU.GetSamples(_source);
+                    Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: samples loaded: {interleavedSamples.Length}, creating table #{_tableNumber}");    
+                    var resInterleaved = _csound.CreateTable(_tableNumber, interleavedSamples);
+                    if (resInterleaved == 0) Debug.Log($"Csound.Unity.Utilities.LoadFiles.TableLoader: Created table {_tableNumber}!");
+                    else Debug.LogError($"Csound.Unity.Utilities.LoadFiles.TableLoader: Cannot create table {_tableNumber}");
+                }   
+            }
         }
     }
 }
